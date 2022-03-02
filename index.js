ea816190fef75b17abf6153a4cf0941377998ebf
Sudharaka <sudharakap@mun.ca>
2022-03-02 13:25:07 -0800

    feat: remove lodash completely to futher reduce bundle size

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const currencyData = require('./currency-data.json');
 const countryData = require('./country-data.json');
-const find = require('lodash.find');
 
 const methods = {
   getEmojiByCountryCode: countryCode => {
@@ -10,7 +9,7 @@ const methods = {
       throw new Error('Expected a country code as the argument (example: CA)');
     }
 
-    return find(countryData, data => {
+    return countryData.find(data => {
       return data.countryCode === countryCode.toUpperCase();
     });
   },
@@ -20,7 +19,7 @@ const methods = {
       throw new Error('Expected a currency code as the argument (example: CAD)');
     }
 
-    return find(currencyData, data => {
+    return currencyData.find(data => {
       return data.currencyCode === currencyCode.toUpperCase();
     });
   },
