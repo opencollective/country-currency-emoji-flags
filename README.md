126909908255fd325a2bd47cb46532d2b9084436
Sudharaka <sudharakap@mun.ca>
2022-03-02 12:31:26 -0800

    feat: add all basic functionality

---
+++ README.md
@@ -0,0 +1,27 @@
+## country-currency-emoji-flags
+
+This small package helps to obtain emojis based on country code ([ISO 3166-1 alpha-2](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)) or the currency code ([ISO 4217](https://en.wikipedia.org/wiki/ISO_4217)).
+
+### Install
+
+> npm install --save country-currency-emoji-flags
+
+### Usage
+
+```js
+var emojiFlags = require('country-currency-emoji-flags');
+
+// Country lookup
+emojiFlags.getEmojiByCountryCode('CA');
+// => { "countryName": "Canada", "emoji": "🇨🇦", countryCode: "CA" }
+
+// Currency lookup
+emojiFlags.getEmojiByCurrencyCode('CAD');
+// => { "currencyName": "Canadian dollar", "emoji": "🇨🇦", currencyCode: "CAD" }
+
+// entire country code set
+emojiFlags.countryData;
+
+// entire currency code set
+emojiFlags.currencyData;
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const currencyData = require('./currency-data.json');
+const countryData = require('./country-data.json');
+const find = require('lodash.find');
+
+const methods = {
+    getEmojiByCountryCode: countryCode => {
+        if (!countryCode) {
+            throw new Error('Expected a country code as the argument (example: CA)');
+        }
+
+        return find(countryData, data => {
+            return data.countryCode === countryCode.toUpperCase();
+        });
+    },
+
+    getEmojiByCurrencyCode: currencyCode => {
+        if (!currencyCode) {
+            throw new Error('Expected a currency code as the argument (example: CAD)');
+        }
+
+        return find(currencyData, data => {
+            return data.currencyCode === currencyCode.toUpperCase();
+        });
+    },
+
+    get currencyData() {
+        return currencyData;
+    },
+
+    get countryData() {
+        return countryData;
+    }
+};
+
+module.exports = methods;
