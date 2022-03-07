bffc18cf4aca8875569ac2d240979d3fbabf4101
Sudharaka <sudharakap@mun.ca>
2022-03-07 11:53:44 -0800

    fix: return null when country code is not found

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const currencyData = require('./currency-data.json');
 const methods = {
   getEmojiByCurrencyCode: currencyCode => {
     if (!currencyCode) {
-      throw new Error('Expected a currency code as the argument (example: CAD)');
+      return null;
     }
 
     return currencyData[currencyCode.toUpperCase()];
